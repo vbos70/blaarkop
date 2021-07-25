e6f40f2abe73ae7f3800f79e22b7310fee0b983e
Victor Bos <victor.bos@gmail.com>
2021-07-25 19:20:22 +0300

    C version added.

---
+++ blmean/blmean.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+
+  double d = atof(argv[1]);
+
+  double n;
+  long long a; // 64 bit machine
+  double b;
+
+  // go_s17J :: Double# -> Int# -> Double# -> Double#
+  for (n = 1, a = 0, b = 0;
+       n <= d;
+       b+=n, n++, a++);
+
+  printf("%f\n", b / a);
+
+  return 0;
+}
---
+++ blmean/blmean_lowlevel.hs
@@ -0,0 +1,27 @@
+-- blmean: compute the mean value of a big list of Doubles.
+--
+-- The code is copied from:
+-- https://donsbot.wordpress.com/2008/06/04/haskell-as-fast-as-c-working-at-a-high-altitude-for-low-level-performance/
+
+-- Compile and run with:
+--
+--     make
+--     time ./blmean_lowlevel 1e9
+--
+
+-- This is called a "low level" version with a recursive definition of
+-- the mean function.
+
+import Text.Printf
+import System.Environment
+
+mean :: Double -> Double -> Double
+mean n m = go 0 0 n
+  where
+    go :: Double -> Int -> Double -> Double
+    go s l x | x > m      = s / fromIntegral l
+             | otherwise  = go (s+x) (l+1) (x+1)
+
+main = do
+  [d] <- map read `fmap` getArgs
+  printf "%f\n" (mean 1 d)
