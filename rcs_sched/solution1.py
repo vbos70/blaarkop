ba4253f1b57d1c271b899c7ae41a3a68e4c00325
Victor Bos <victor.bos@gmail.com>
2021-08-15 22:05:13 +0300

    Added more job schedulers.

--- rcs_sched/solution1.py
+++ rcs_sched/solution1.py
@@ -50,5 +50,81 @@ def job_scheduler(max_steps):
         s.configure({Processor.P6 : s.cur_alloc[mirror]})
     print(s)
         
+
+def prev(p: Processor):
+    return Processor(p.value-1) if p.value>1 else Processor(len(Processor))
+    
+def job_scheduler2(max_steps):
+    
+    mirror = Processor.P2
+    jobs = [j for j in Job ]
+    processors = [p for p in Processor if p != mirror]
+    pj_tuples = zip(processors, jobs)
+    red_job = Job(mirror.value-1 if mirror.value>1 else 5)
+    alloc = add({ p : j for (p,j) in pj_tuples}, { mirror: red_job})
+    s = State(processors=processors,
+              T0=0,
+              alloc=alloc
+    )
+    n = 0
+    while n<max_steps:
+        print(s, mirror)
+        n += 1
+        if mirror<len(Processor):
+            mirror = Processor(mirror.value + 2)
+        else:
+            mirror = Processor.P2
+            jobs = jobs[1:] + [jobs[0]]
+        processors = [p for p in Processor if p != mirror]
+        alloc0 = { p: j for p,j in zip(processors, jobs) }
+        red_job = Job(alloc[mirror])
+        alloc = add(alloc0, { mirror: red_job})
+        s.configure(alloc)
+    print(s)
+
+def prev_proc(p:Processor):
+    if p.value-1 < 1:
+        return Processor(len(Processor))
+    else:
+        return Processor(p.value-1)
+    
+def next_proc(p:Processor):
+    if p.value+1 > len(Processor):
+        return Processor(1)
+    else:
+        return Processor(p.value+1)
+    
+def job_scheduler3(max_steps, mirror):
+    mirror = Processor(mirror)
+    
+    alloc = { p : None for p in Processor }
+    p_idx = 1
+    for j in Job:
+        if mirror.value == j.value:
+            p_idx += 1
+
+        alloc[Processor(p_idx)] = j
+        p_idx += 1
+    alloc[mirror] = alloc[prev_proc(mirror)]
+    
+    s = State([p for p in Processor],
+              T0=0,
+              alloc=alloc
+    )
+    n = 0
+    while n<max_steps:
+        print(s, mirror)
+        n += 1
+
+        new_alloc = alloc
+        new_alloc[mirror] = alloc[next_proc(mirror)]
+        new_alloc[next_proc(mirror)] = alloc[next_proc(next_proc(mirror))]
         
+        if mirror<len(Processor)-1:
+            mirror = Processor(mirror.value + 2)
+        else:
+            mirror = Processor.P2
+        s.configure(new_alloc)
+    print(s)
+
     
