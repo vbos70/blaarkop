812eaea01c6e7cf6b81f5d650b32e32cb77761aa
Victor Bos <victor.bos@gmail.com>
2022-04-10 14:01:55 +0300

    Prime factorization added.

--- primes/src/Primes.hs
+++ primes/src/Primes.hs
@@ -18,3 +18,20 @@ sieve :: [Int] -> [Int]
 sieve [] = []
 sieve (nextPrime:rest) = nextPrime : sieve noFactors
   where noFactors = filter (not . (==0) . (`mod` nextPrime)) rest
+
+-- Compute prime factors of n with 2 <= n < upperBound
+-- Assumption: the 2nd argument is a list of prime numbers.
+unsafePrimeFactors :: Int -> [Int] -> [Int]
+unsafePrimeFactors 0 [] = []
+unsafePrimeFactors n [] = []
+unsafePrimeFactors n (next:rest) = if n `mod` next == 0
+                                   then next:unsafePrimeFactors (n `div` next) (next:rest)
+                                   else unsafePrimeFactors n rest
+                                          
+-- Compute prime factors of n :: Int
+primeFactors :: Int -> Maybe [Int]
+primeFactors n | n < 2 = Nothing
+               | n >= upperBound = Nothing
+               | otherwise = Just (unsafePrimeFactors n primesLessThanN)
+  where primesLessThanN = filter (<= n) primes
+        
