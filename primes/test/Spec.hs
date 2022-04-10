2cf3dd55edb6f7a33859cd28ed8f41b25a133a27
Victor Bos <victor.bos@gmail.com>
2022-04-10 10:51:14 +0300

    Testing primes and compisite numbers.

--- primes/test/Spec.hs
+++ primes/test/Spec.hs
@@ -1,2 +1,31 @@
+import Test.QuickCheck
+import Primes
+
+import Data.Maybe
+
+-- Check border-cases
+prop_validPrimesOnly val = if val < 2 || val >= upperBound
+                       then result == Nothing
+                       else isJust result
+  where result = isPrime val
+
+
+-- Check if n is a prime when isPrime n == Just True
+prop_primesArePrime val = if result == Just True
+                          then length divisors == 0
+                          else True
+  where result = isPrime val
+        divisors = filter ((== 0) . (val `mod`)) [2 .. (val - 1)]
+
+-- Check if n is composite if isPrime n == Just False
+prop_nonPrimesAreComposite val = if result == Just False
+                                 then length divisors > 0
+                                 else True
+  where result = isPrime val
+        divisors = filter ((== 0) . (val `mod`)) [2 .. (val - 1)]
+        
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  quickCheck prop_validPrimesOnly
+  quickCheckWith stdArgs { maxSuccess = 1000} prop_primesArePrime
+  quickCheckWith stdArgs { maxSuccess = 1000} prop_nonPrimesAreComposite
