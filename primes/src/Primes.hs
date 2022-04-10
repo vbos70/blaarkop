16c9e3f89469ccbe0055ed852bd962bdadfe1a02
Victor Bos <victor.bos@gmail.com>
2022-04-10 10:06:45 +0300

    Sieve of Eratosthenes implemented

--- primes/src/Primes.hs
+++ primes/src/Primes.hs
@@ -2,3 +2,9 @@ module Primes where
 
 primes :: [Int]
 primes = [1 .. ] -- to be refactored later
+
+-- Implementation of the sieve of Eratosthenes
+sieve :: [Int] -> [Int]
+sieve [] = []
+sieve (nextPrime:rest) = nextPrime : sieve noFactors
+  where noFactors = filter (not . (==0) . (`mod` nextPrime)) rest
