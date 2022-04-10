b7224d24f10b7aae0b31f80ee2d7b936f46b1e18
Victor Bos <victor.bos@gmail.com>
2022-04-10 10:27:36 +0300

    Defined the prime test

--- primes/src/Primes.hs
+++ primes/src/Primes.hs
@@ -1,9 +1,17 @@
 module Primes where
 
--- Infinite (?) list of all primes
--- Note: it isn't infinite, because it computes only primes <= maxBound :: Int
+-- Upper bound for primes check
+upperBound = 10000 :: Int
+
+-- Determine if a number is prime.
+isPrime :: Int -> Maybe Bool
+isPrime n | n < 2  = Nothing
+          | n < upperBound = Just (n `elem` primes)
+          | otherwise = Nothing
+
+-- List of all primes less than upperBound
 primes :: [Int]
-primes = sieve [2 .. ]
+primes = sieve [2 .. upperBound]
 
 -- Implementation of the sieve of Eratosthenes
 sieve :: [Int] -> [Int]
