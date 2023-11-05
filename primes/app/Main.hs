7b2d2b09a49729042f817302881de7800fc844ad
Victor Bos <victor.bos@gmail.com>
2023-11-05 11:38:19 +0200

    cli for primes

--- primes/app/Main.hs
+++ primes/app/Main.hs
@@ -1,6 +1,21 @@
 module Main where
 
-import Primes
+import Primes (primeFactors)
+import System.Environment ( getArgs )
+import Control.Category (Category(id))
+
+formatInts :: Maybe [Int] -> String
+formatInts Nothing = ""
+formatInts (Just [i]) = show i
+formatInts (Just (i:is)) = show i <> ", " <> formatInts (Just is)
+
+formatPrimeFactors [] = "no input"
+formatPrimeFactors [x] = show x <> ": " <> formatInts (primeFactors x)
+formatPrimeFactors (x:xs) = show x <> ": " <> formatInts (primeFactors x) <> "\n" <> formatPrimeFactors xs
+
 
 main :: IO ()
-main = return ()
+main = do
+    input <- getArgs
+    putStrLn $ formatPrimeFactors (map read input)
+    --putStrLn("Prime factors of 53622: " <> (show $ primeFactors 53662)) 
