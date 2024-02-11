1cbc91890ae818f0c90c5bb816b16579eaa766b7
Victor Bos <victor.bos@gmail.com>
2024-02-11 16:31:24 +0200

    Summary-only output

--- eqrw/test_eqrw.py
+++ eqrw/test_eqrw.py
@@ -6,7 +6,8 @@ num_tests = 0
 num_failed_tests = 0
 test_output = []
 suppress_test_output = False
-named_tests = {}
+summary_only = False
+test_funcs = []
 
 def test_print(*args):
     global suppress_test_output
@@ -14,50 +15,74 @@ def test_print(*args):
         test_output.extend(list(str(a) for a in args))
 
 def test(func):
-    global named_tests
+    global test_funcs
 
     def inner():
         global num_tests
         global num_failed_tests
         global test_output
-        print(f'# Test {func.__name__}: ', end='', flush=True)
+        global summary_only
+
+        if not summary_only:
+            print(f'# Test {func.__name__}: ', end='', flush=True)
         try:            
             test_output = []
             num_tests += 1
             func()
-            print(f'passed.')
+            if not summary_only:
+                print(f'passed.')
             if len(test_output)>0:
                 print("Test output:\n" + "\n".join(test_output))
         except AssertionError as ae:
             num_failed_tests += 1
             print(f'Failed:')
             print_exception(ae, limit=-1)
+        except Exception as e:
+            print(f'Error:')
+            print_exception(e)
 
-    named_tests[func.__name__] = inner
+    test_funcs.append(inner)
     return inner
 
 def test_summary():
     global num_tests
     global num_failed_tests
-    return f'# SUMMARY: Tests: {num_tests}. Passed: {num_tests-num_failed_tests}. Failed: {num_failed_tests}.'        
+    result = [f'# SUMMARY:{num_tests} tests']
+    if num_failed_tests > 0:
+        result.append(f':{num_tests-num_failed_tests} passed:{num_failed_tests} failed.')
+    else:
+        result.append(':all passed.')
+    return "".join(result)
 
 
-def run_tests(*tfuncs, new_suppress_test_output = False):
+def run_tests(*selected, print_summary_only=False, new_suppress_test_output = False):
     global num_tests
     global num_failed_tests
     global suppress_test_output
+    global test_funcs
+    global summary_only
 
+    # remember global output suppression switch
     sto = suppress_test_output
+    so = summary_only
+
     suppress_test_output = new_suppress_test_output
+    summary_only = print_summary_only
+
     num_tests = 0
     num_failed_tests = 0
-    if len(tfuncs) == 0:
-        for n in named_tests:
-            named_tests[n]()
-    else:
-        for tf in named_tests.values():
-            if tf in tfuncs:
-                tf()
+
+    tf_iter = test_funcs
+    if len(selected) > 0:
+        tf_iter = (tf for tf in test_funcs if tf in selected)
+
+    for tf in tf_iter:
+        tf()
+
+    # restore global output suppression
+    suppress_test_output = sto
+    summary_only = so
+
 ############################################
                 
 @test
@@ -246,9 +271,9 @@ def test_proof_summary():
     test_print()
 
 
-run_tests(new_suppress_test_output=True)
+run_tests(print_summary_only=True, new_suppress_test_output=True)
 print(test_summary())
 
-run_tests(test_proof_summary, new_suppress_test_output=True)
-print(test_summary())
+#run_tests(test_proof_summary, new_suppress_test_output=True)
+#print(test_summary())
 
