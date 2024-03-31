1962947fca3979be7da26e5633aa225ea7c00e1a
Victor Bos <victor.bos@gmail.com>
2024-03-31 17:19:32 +0300

    Re-implemented EqProof for new approach

--- eqrw/process_theory.py
+++ eqrw/process_theory.py
@@ -1,5 +1,8 @@
 from utils import *
 from itertools import chain
+from process import *
+from eqrw import z3_prove
+
 
 class Theory(AttrDict):
     """Theories represent equational theories with Python dictionaries. 
@@ -81,3 +84,106 @@ class Theory(AttrDict):
  
         return "\n".join(result)
         
+
+class EqProofException(Exception): pass
+
+class EqProof():
+    def __init__(self, e: CoreProcess):
+        ''' Create an equational proof starting with `CoreProcess` `e`.
+        Since `e` is the initial step of the proof, its justification is set to `None`.
+
+        If `e` is not of type `CoreProcess`, an `EqProofException` is raised.
+        '''
+        if not isinstance(e, CoreProcess):
+            raise EqProofException(f"Expected e : Process, got e : {type(e)}")
+        
+        self.step = [e]
+        self.justification = [None]
+        
+
+    def step_is_valid(self, i=None):
+        ''' Checks if step `i` in this proof is valid, meaning it can be proven by z3.
+
+        Returns `True` if z3 can prove step `i`, `False` otherwise.
+
+        If no `i` is given (`i==None`), the last step of the proof is checked.
+
+        An `EqProofException` is raised if `i<0` or `i>=len(self)`. 
+        '''
+        if i is None:
+            i = len(self.step)-1
+        if i < 0:
+            raise EqProofException(f"step index shall be at least 0, got {i}")
+        if not i < len(self.step):
+            raise EqProofException(f"step index shall less than the len(self): {len(self.step)}, got {i}")
+        if i == 0:
+            return True
+        return z3_prove(self.step[i-1].z3expr == self.step[i].z3expr, [eq.z3expr for eq in self.justification[i]])
+    
+
+    def _extend_(self, e, cs):
+        '''Extends this proof by Process `e` and justification `*cs`.
+
+        Returns `self`.
+
+        Raises a `ProofException` if z3 cannot prove `e` with the justification `*cs`.
+        '''
+        equations = []
+        variables = []
+        for c in cs:
+            if type(c) == Theory:
+                equations.extend(c.values())
+                variables.extend(c.variables())
+            elif type(c) == ProcessEquality:
+                equations.append(c)
+                variables.extend(v for v in c.lhs.vars())
+                variables.extend(v for v in c.rhs.vars())
+            else:
+                raise EqProofException(f"Equation expected, got {c} : {type(c)}")
+
+        z3variables = list(set(v.z3expr for v in variables))
+        z3equations = [ForAll(z3variables, e.z3expr) for e in equations]
+        step = self.step[-1] == e
+
+        if not z3_prove(step.z3expr, z3equations):
+            raise EqProofException(f"Cannot proof {self.step[-1]} == {e} from {', '.join(str(eq) for eq in equations)}")
+        self.step.append(e)
+        self.justification.append(cs)
+        return self
+
+
+    def __iadd__(self, other):
+        '''This defines the in-place `+=` operator for EqProof objects.
+
+        The effect is equal to `self._extend_(other[0], other[1:]).
+        '''
+        if type(other) is not tuple:
+            other = (other,)
+        return self._extend_(other[0], other[1:])
+
+
+    def add(self, e, *cs):
+        '''This is equivalent to `self.__iadd__(tuple([e] + cs))`.'''
+        return self._extend_(e, cs)
+
+
+    def append(self, e, *cs):
+        '''This is equivalent to `self.__iadd__(tuple([e] + cs))`.'''
+        return self._extend_(e, cs)
+
+    def eq_proof_str(self, indent = 0):
+        return "\n".join(merge(((" " * indent)+ "  " + str(e) for e in self.step), 
+                               ((" " * indent)+"= {" + ", ".join(str(c) for c in cs if not isinstance(c, Theory)) + "}" for cs in self.justification[1:]),
+                               fillvalue=''))
+
+    def __getitem__(self, i):
+        if type(i) == slice:
+            result = EqProof(self[i.start])
+            result.step = self.step[i]
+            result.justification = self.justification[i]
+            return result
+        return EqProof(self.step[i])
+    
+    def __len__(self):
+        return len(self.step)
+
--- eqrw/test_process_theory.py
+++ eqrw/test_process_theory.py
@@ -31,6 +31,47 @@ def test_process_theory1():
     assert len(TH1.atoms()) == 2
     test_print(TH1)
 
+
+@test
+def test_proof():
+    x, y, z = vars('x, y, z')
+
+    zero, one = atoms('zero, one')
+
+    TH1 = Theory(
+        variables = [x,y,z],
+
+        atoms = [zero, one],
+
+        AX1 = zero * x == zero,
+        AX2 = one * x == x,
+        AX3 = x * one == x,
+        AX4 = x + zero == x,
+        AX5 = x + y == y + x,
+        AX6 = x + x == x,
+        AX7 = (x + y) + x == x + (y + z),
+        AX8 = (x * y) * z == x * (y * z),
+        AX9 = (x + y) * z == x * z + y * z
+        )
+
+    prf = EqProof(zero * one)
+    prf += zero, TH1.AX1
+
+    test_print(prf.eq_proof_str())
+
+    prf = EqProof(zero * one * one)
+    prf += zero * one, TH1.AX1
+    prf += zero, TH1.AX1
+
+    test_print(prf.eq_proof_str())
+
+    prf = EqProof(zero * (one * one))
+    prf += zero * one, TH1
+    prf += zero, TH1.AX1
+
+    test_print(prf.eq_proof_str())
+
+
 if __name__ == '__main__':
     run_tests(print_summary_only=False, new_suppress_test_output=False)
     print(test_summary())
