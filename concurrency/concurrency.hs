20b65c376297fdd824908b747f5c8b0b3c3f82aa
Victor Bos <victor.bos@gmail.com>
2024-12-08 16:12:04 +0200

    Added Law and calculations with Law

--- concurrency/concurrency.hs
+++ concurrency/concurrency.hs
@@ -12,137 +12,124 @@ data ProcessGraph =
     | Mrg ProcessGraph ProcessGraph
     | Lmr ProcessGraph ProcessGraph
     | Cmr ProcessGraph ProcessGraph
+    | T ProcessGraph
     deriving (Show, Eq, Ord)
 
+type Law = ProcessGraph -> ProcessGraph
 
 x = Proc "x" (Seq (Act "a") x) -- X = a ; X
 
-eval :: ProcessGraph -> ProcessGraph
-eval (Seq Empty p) = eval p
-eval (Seq Deadlock p) = Deadlock
-eval (Seq p1 p2) = Seq (eval p1) (eval p2) 
-eval (Alt Deadlock p) = eval p
-eval (Alt p Deadlock) = eval p
-eval (Alt p1 p2) = Alt (eval p1) (eval p1) 
-eval p = p -- otherwise
+eval :: Law -> ProcessGraph -> ProcessGraph
+eval l (Seq p1 p2) = Seq (eval l p1) (eval l p2) 
+eval l (Alt p1 p2) = Alt (eval l p1) (eval l p2) 
+eval l (Mrg p1 p2) = Mrg (eval l p1) (eval l p2) 
+eval l (Lmr p1 p2) = Lmr (eval l p1) (eval l p2) 
+eval l (Cmr p1 p2) = Lmr (eval l p1) (eval l p2) 
+eval l (T p) = l p
+eval l p = p -- otherwise
+
+neg_eval :: Law -> ProcessGraph -> ProcessGraph
+neg_eval l (Seq p1 p2) = Seq (neg_eval l p1) (neg_eval l p2) 
+neg_eval l (Alt p1 p2) = Alt (neg_eval l p1) (neg_eval l p2) 
+neg_eval l (Mrg p1 p2) = Mrg (neg_eval l p1) (neg_eval l p2) 
+neg_eval l (Lmr p1 p2) = Lmr (neg_eval l p1) (neg_eval l p2) 
+neg_eval l (Cmr p1 p2) = Cmr (neg_eval l p1) (neg_eval l p2) 
+neg_eval l (T p) = p
+neg_eval l p = p -- otherwise
 
 
-merge :: ProcessGraph -> ProcessGraph -> ProcessGraph
-merge p q = Alt (cmerge p q) (Alt (lmerge p q) (lmerge q p)) 
-
-lmerge :: ProcessGraph -> ProcessGraph -> ProcessGraph
-lmerge Deadlock p = p
-lmerge Empty p = p
-lmerge (Act s) p = Seq (Act s) p
-lmerge (Seq (Act s) p) q = Seq (Act s) (merge p q)
-lmerge (Alt p q) r = Alt (lmerge p r) (lmerge q r)
-
-cmerge :: ProcessGraph -> ProcessGraph -> ProcessGraph
-cmerge (Act s) (Act t) = Act (s ++ "," ++ t)
-cmerge (Seq (Act s) p) q = cmerge (Act s) q
-cmerge p (Seq (Act s) q) = cmerge p (Act s)
-cmerge (Alt p q) r = Alt (cmerge p r) (cmerge q r)
-cmerge _ _ = Deadlock -- otherwise
-
-act :: String -> ProcessGraph
-act = Act
-
-acts = map act
-
-sqn [] = Empty
-sqn (a: as) = Seq a (sqn as)
-
-alt [] = Deadlock
-alt (a:as) = Alt a (alt as)
 
 -- delta ; x = x
-fwdSeqDeadlockX :: ProcessGraph -> ProcessGraph
-fwdSeqDeadlockX (Seq Deadlock x) = x
+fwdSeqDeadlockX :: Law
+fwdSeqDeadlockX (Seq Deadlock x) = Deadlock
 fwdSeqDeadlockX x = x
 
-revSeqDeadlockX :: ProcessGraph -> ProcessGraph
+revSeqDeadlockX :: Law
 revSeqDeadlockX x = Seq Deadlock x
 
 -- empty ; x = x
-fwdSeqEmptyX :: ProcessGraph -> ProcessGraph
+fwdSeqEmptyX :: Law
 fwdSeqEmptyX (Seq Empty x) = x
 fwdSeqEmptyX x = x
 
-revSeqEmptyX :: ProcessGraph -> ProcessGraph
+revSeqEmptyX :: Law
 revSeqEmptyX x = Seq Empty x
 
 -- x ; empty = x
-fwdSeqXEmpty :: ProcessGraph -> ProcessGraph
+fwdSeqXEmpty :: Law
 fwdSeqXEmpty (Seq x Empty) = x
 fwdSeqXEmpty x = x
 
-revSeqXEmpty :: ProcessGraph -> ProcessGraph
+revSeqXEmpty :: Law
 revSeqXEmpty x = Seq x Empty
 
 -- (x ; y) ; z = x ; (y ; z)
-fwdSeqSeq :: ProcessGraph -> ProcessGraph
+fwdSeqSeq :: Law
 fwdSeqSeq (Seq (Seq x y) z) = (Seq x (Seq y z)) 
 fwdSeqSeq x = x
 
-revSeqSeq :: ProcessGraph -> ProcessGraph
+revSeqSeq :: Law
 revSeqSeq (Seq x (Seq y z))  = (Seq (Seq x y) z)
 revSeqSeq x = x
 
 -- delta + x = x
-fwdAltDeadlockX :: ProcessGraph -> ProcessGraph
+fwdAltDeadlockX :: Law
 fwdAltDeadlockX (Alt Deadlock x) = x
 fwdAltDeadlockX x = x
 
-revAltDeadlockX :: ProcessGraph -> ProcessGraph
+revAltDeadlockX :: Law
 revAltDeadlockX x = (Alt Deadlock x)
 
 -- x + delta = x
-fwdAltXDeadlock :: ProcessGraph -> ProcessGraph
+fwdAltXDeadlock :: Law
 fwdAltXDeadlock (Alt x Deadlock) = x
 fwdAlXDeadlock x = x
 
-revAltXDeadlock :: ProcessGraph -> ProcessGraph
+revAltXDeadlock :: Law
 revAltXDeadlock x = (Alt x Deadlock)
 
 -- (x + y) + z = x + (y + z)
-fwdAltAlt :: ProcessGraph -> ProcessGraph
+fwdAltAlt :: Law
 fwdAltAlt (Alt (Alt x y) z) = (Alt x (Alt y z)) 
 fwdAltAlt x = x
 
-revAltAlt :: ProcessGraph -> ProcessGraph
+revAltAlt :: Law
 revAltAlt (Alt x (Alt y z))  = (Alt (Alt x y) z)
 revAltAlt x = x
 
 -- (x + y) ; z = (x ; z) + (y ; z)
-fwdSeqAltXYZ :: ProcessGraph -> ProcessGraph
+fwdSeqAltXYZ :: Law
 fwdSeqAltXYZ (Seq (Alt x y) z) = Alt (Seq x z) (Seq y z)
 fwdSeqAltXYZ x = x
 
-revSeqAltXYZ :: ProcessGraph -> ProcessGraph
+revSeqAltXYZ :: Law
 revSeqAltXYZ (Alt (Seq x z1) (Seq y z2))
     | z1 == z2 = Seq (Alt x y) z1
     | otherwise = Alt (Seq x z1) (Seq y z2)
 revSeqAltXYZ x = x
 
-step :: ProcessGraph -> ProcessGraph -> Bool
-step x y
-    | x == y = True
+step :: (Law, ProcessGraph) -> (Law, ProcessGraph) -> Bool
+step (lx, px)  (ly, py)
+    | eval lx px == py = True
+    | px == neg_eval ly py = True
     | otherwise = False
 
-calc:: [ProcessGraph] -> Bool
+calc:: [(Law, ProcessGraph)] -> Bool
 calc [] = True
 calc [x] = True
 calc (x : y : xs)
     | step x y = calc (y : xs)
     | otherwise = False
 
+noop :: Law
+noop p = p
 
 c1 = [
-    Alt (Seq Deadlock (Seq Deadlock Empty)) Empty,
-    Alt (Seq Deadlock (fwdSeqDeadlockX (Seq Deadlock Empty))) Empty,
-    Alt (Seq Deadlock Deadlock) Empty,
-    Alt (fwdSeqDeadlockX (Seq Deadlock Deadlock)) Empty,
-    Alt Deadlock Empty,
-    fwdAltDeadlockX (Alt Deadlock Empty),
-    Empty
+    (noop, Alt (Seq Deadlock (Seq Deadlock Empty)) Empty),
+    (fwdSeqDeadlockX, Alt (Seq Deadlock (T (Seq Deadlock Empty))) Empty),
+    (noop, Alt (Seq Deadlock Deadlock) Empty),
+    (fwdSeqDeadlockX, Alt (T  (Seq Deadlock Deadlock)) Empty),
+    (noop, Alt Deadlock Empty),
+    (fwdAltDeadlockX, T (Alt Deadlock Empty)),
+    (noop, Empty)
     ]
